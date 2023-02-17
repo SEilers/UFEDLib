63b61f8eb76a5d86f1b30afd9cf8b42347ee1008
Sönke Eilers <soenke.eilers@gmail.com>
2023-02-17 16:50:17 +0100

    Projektdateien hinzufügen.

---
+++ UFEDLib/Chat.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+
+namespace UFEDLib
+{
+    public class Chat
+    {
+        public String Id { get; set; }
+
+        public String Source { get; set; }
+
+        public DateTime StartTime { get; set; }
+
+        public DateTime LastActivity { get; set; }
+        public List<Party> Participants { get; set; } = new List<Party>();
+
+        public List<InstantMessage> InstantMessages { get; set; } = new List<InstantMessage>();
+
+        public String Account { get; set; }
+    }
+}
---
+++ UFEDLib/Party.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UFEDLib
+{
+    public class Party
+    {
+        public string Name { get; set; }
+
+        public String Identifier { get; set; }
+
+        public String Role { get; set; }
+
+        public bool IsPhoneOwner { get; set; }
+
+        public override string ToString()
+        {
+            return Name + " " + Identifier + " " + Role;
+        }
+
+    }
+}
