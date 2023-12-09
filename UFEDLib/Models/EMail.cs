737c79c3b16bc3b23765a4b04721605a39364736
Sönke Eilers <soenke.eilers@gmail.com>
2023-12-09 20:20:49 +0100

    deleted superflous models

--- UFEDLib/Models/EMail.cs
+++ UFEDLib/Models/EMail.cs
@@ -8,5 +8,28 @@ namespace UFEDLib.Models
 {
     internal class EMail
     {
+        public String Folder { get; set; }
+
+        public String Status { get; set; }
+
+        public Party From { get; set; }
+
+        public List<Party> To { get; set; }
+
+        public List<Party> Cc { get; set; }
+
+        public List<Party> Bcc { get; set; }
+
+        public String Subject { get; set; }
+
+        public String Body { get; set; }
+
+        public DateTime TimeStamp { get; set; }
+
+        public String Priority { get; set; }    
+
+        public List<Attachment> Attachments { get; set; }
+
+        public String Source { get; set; }  
     }
 }
