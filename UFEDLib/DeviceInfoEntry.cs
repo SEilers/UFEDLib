621e236d447d1b4396990f52efd26c55ec3dd930
Sönke Eilers <soenke.eilers@gmail.com>
2025-02-04 19:57:58 +0100

    user model

---
+++ UFEDLib/DeviceInfoEntry.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace UFEDLib
+{
+    public class DeviceInfoEntry : ModelBase, IUfedModelParser<DeviceInfoEntry>
+    {
+        public static string GetXmlModelType()
+        {
+            return "DeviceInfoEntry";
+        }
+
+        #region fields
+        public string UserMapping { get; set; }
+
+        public string Source { get; set; }
+
+        public string EntryName { get; set; }
+
+        public string EntryValue { get; set; }
+
+        public string EntryCategory { get; set; }
+        #endregion
+
+        #region Parsers
+        public static DeviceInfoEntry ParseModel(XElement element, bool debugAttributes = false)
+        {
+            XNamespace xNamespace = "http://pa.cellebrite.com/report/2.0";
+
+            DeviceInfoEntry result = new DeviceInfoEntry();
+
+            var fieldElements = element.Elements(xNamespace + "field");
+            var multiFieldElements = element.Elements(xNamespace + "multiField");
+            var multiModelFieldElements = element.Elements(xNamespace + "multiModelField");
+
+            foreach (var field in fieldElements)
+            {
+                switch (field.Attribute("name").Value)
+                {
+                    case "UserMapping":
+                        result.UserMapping = field.Value.Trim();
+                        break;
+
+                    case "Source":
+                        result.Source = field.Value.Trim();
+                        break;
+
+                    case "EntryName":
+                        result.EntryName = field.Value.Trim();
+                        break;
+
+                    case "EntryValue":
+                        result.EntryValue = field.Value.Trim();
+                        break;
+
+                    case "EntryCategory":
+                        result.EntryCategory = field.Value.Trim();
+                        break;
+
+                    default:
+                        if (debugAttributes)
+                        {
+                            Console.WriteLine("DeviceInfoEntry Parser: Unknown field: " + field.Attribute("name").Value);
+                        }
+                        break;
+                }
+            }
+
+            foreach (var multiField in multiFieldElements)
+            {
+                switch (multiField.Attribute("name").Value)
+                {
+                    default:
+                        if (debugAttributes)
+                        {
+                            Console.WriteLine("DeviceInfoEntry Parser: Unknown multiField: " + multiField.Attribute("name").Value);
+                        }
+                        break;
+                }
+            }
+
+            foreach (var multiModelField in multiModelFieldElements)
+            {
+                switch (multiModelField.Attribute("name").Value)
+                {
+                    default:
+                        if (debugAttributes)
+                        {
+                            Console.WriteLine("DeviceInfoEntry Parser: Unknown multiModelField: " + multiModelField.Attribute("name").Value);
+                        }
+                        break;
+                }
+            }
+
+            return result;
+        }
+
+        public static List<DeviceInfoEntry> ParseMultiModel(XElement element, bool debugAttributes = false)
+        {
+            XNamespace xNamespace = "http://pa.cellebrite.com/report/2.0";
+            List<DeviceInfoEntry> result = new List<DeviceInfoEntry>();
+
+            IEnumerable<XElement> dieElements = element.Elements(xNamespace + "model").Where(x => x.Attribute("type").Value == "DeviceInfoEntry");
+
+            foreach (XElement dieElement in dieElements)
+            {
+                DeviceInfoEntry die = ParseModel(dieElement, debugAttributes);
+                result.Add(die);
+            }
+
+            return result;
+        }
+        #endregion
+    }
+}
---
+++ UFEDLib/User.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace UFEDLib
+{
+    public class User : ModelBase, IUfedModelParser<User>
+    {
+        public static string GetXmlModelType()
+        {
+            return "User";
+        }
+
+        #region fields
+        public string UserMapping { get; set; }
+
+        public string Identifier { get; set; }
+
+        public long SerialNumber { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime TimeLastLoggedIn { get; set; }
+
+        public string UserType { get; set; }
+        #endregion
+
+
+        #region Parsers
+        public static User ParseModel(XElement element, bool debugAttributes = false)
+        {
+            XNamespace xNamespace = "http://pa.cellebrite.com/report/2.0";
+
+            User result = new User();
+
+            var fieldElements = element.Elements(xNamespace + "field");
+            var multiFieldElements = element.Elements(xNamespace + "multiField");
+            var multiModelFieldElements = element.Elements(xNamespace + "multiModelField");
+
+            foreach (var field in fieldElements)
+            {
+                switch (field.Attribute("name").Value)
+                {
+                    case "UserMapping":
+                        result.UserMapping = field.Value.Trim();
+                        break;
+
+                    case "Identifier":
+                        result.Identifier = field.Value.Trim();
+                        break;
+
+                    case "SerialNumber":
+                        if (field.Value.Trim() != "")
+                            result.SerialNumber = long.Parse(field.Value.Trim());
+                        break;
+
+                    case "Name":
+                        result.Name = field.Value.Trim();
+                        break;
+
+                    case "UserType":
+                        result.UserType = field.Value.Trim();
+                        break;
+
+                    case "TimeLastLoggedIn":
+                        if (field.Value.Trim() != "")
+                            result.TimeLastLoggedIn = DateTime.Parse(field.Value.Trim());
+                        break;
+
+                    default:
+                        if (debugAttributes)
+                        {
+                            Console.WriteLine("User Parser: Unknown field: " + field.Attribute("name").Value);
+                        }
+                        break;
+                }
+            }
+
+            foreach (var multiField in multiFieldElements)
+            {
+                switch (multiField.Attribute("name").Value)
+                {
+                    default:
+                        if (debugAttributes)
+                        {
+                            Console.WriteLine("User Parser: Unknown multiField: " + multiField.Attribute("name").Value);
+                        }
+                        break;
+                }
+            }
+
+            foreach (var multiModelField in multiModelFieldElements)
+            {
+                switch (multiModelField.Attribute("name").Value)
+                {
+                    default:
+                        if (debugAttributes)
+                        {
+                            Console.WriteLine("User Parser: Unknown multiModelField: " + multiModelField.Attribute("name").Value);
+                        }
+                        break;
+                }
+            }
+
+            return result;
+        }
+
+        public static List<User> ParseMultiModel(XElement element, bool debugAttributes = false)
+        {
+            XNamespace xNamespace = "http://pa.cellebrite.com/report/2.0";
+            List<User> result = new List<User>();
+
+            IEnumerable<XElement> uElements = element.Elements(xNamespace + "model").Where(x => x.Attribute("type").Value == "User");
+
+            foreach (XElement uElement in uElements)
+            {
+                User aul = ParseModel(uElement, debugAttributes);
+                result.Add(aul);
+            }
+
+            return result;
+        }
+        #endregion
+    }
+}
