36abc30aa02804cfdbaad2bcb1671f66126f3589
Sönke Eilers <soenke.eilers@gmail.com>
2025-03-05 21:25:42 +0100

    ProjectAttributes and MetaData

--- UFEDLib/ProjectAttributes.cs
+++ UFEDLib/ProjectAttributes.cs
@@ -53,140 +53,32 @@ namespace UFEDLib
 
             FileInfo fileInfo = new FileInfo(filename);
             long reportSize = fileInfo.Length;
-            long currentPosition = 0;
-            int lastPercent = 0;
 
             try
             {
-                using (ZipArchive zip = ZipFile.OpenRead(filename))
+                if(Path.GetExtension(filename).ToLower() == ".ufdr")
                 {
-                    var report = zip.GetEntry("report.xml");
-
-                    if (report == null)
+                    using (ZipArchive zip = ZipFile.OpenRead(filename))
                     {
-                        Console.WriteLine("report.xml not found in the ufdr file");
-                        return null;
-                    }
+                        var report = zip.GetEntry("report.xml");
 
-
-                    using (Stream reportStream = report.Open())
-                    {
-                        using (StreamReader sr = new StreamReader(reportStream))
+                        if (report == null)
                         {
-                            XmlReaderSettings xmlReaderSettings = new XmlReaderSettings
-                            {
-                                CheckCharacters = false
-                            };
-         
-                            using (XmlReader reader = XmlReader.Create(sr, xmlReaderSettings))
-                            {
-                                ProjectAttributes ufedProjectAttributes = new ProjectAttributes();
-
-                                var nsmgr = new XmlNamespaceManager(new NameTable());
-                                nsmgr.AddNamespace("a", "http://pa.cellebrite.com/report/2.0");
-                                XNamespace xNamespace = "http://pa.cellebrite.com/report/2.0";
-
-                                while (reader.Read())
-                                {
-                                    try
-                                    {
-                                        if (reader.Name == "project" && reader.NodeType != XmlNodeType.EndElement)
-                                        {
-                                            ufedProjectAttributes.ProjectId = reader.GetAttribute("id");
-                                            ufedProjectAttributes.ProjectName = reader.GetAttribute("name");
-                                            ufedProjectAttributes.NodeCount = int.Parse(reader.GetAttribute("NodeCount"));
-                                            ufedProjectAttributes.ModelCount = int.Parse(reader.GetAttribute("ModelCount"));
-                                            ufedProjectAttributes.ReportVersion = reader.GetAttribute("reportVersion");
-                                        }
-
-                                        if (reader.Depth == 1 && reader.Name == "metadata" && reader.GetAttribute("section") == "Additional Fields")
-                                        {
-                                            XmlReader attReader = reader.ReadSubtree();
-                                            XElement attNode = XElement.Load(attReader);
-
-                                            IEnumerable<XElement> atributes = attNode.Descendants();
-
-                                            foreach (XElement att in atributes)
-                                            {
-                                                string name = (string)att.Attribute("name");
-                                                string value = att.Value;
-
-                                                ufedProjectAttributes.AdditionalFields.Add(Tuple.Create(name, value));
-                                            }
-                                        }
-                                        else if (reader.Depth == 1 && reader.Name == "metadata" && reader.GetAttribute("section") == "Extraction Data")
-                                        {
-                                            XmlReader attReader = reader.ReadSubtree();
-                                            XElement attNode = XElement.Load(attReader);
-
-                                            IEnumerable<XElement> atributes = attNode.Descendants();
-
-                                            foreach (XElement att in atributes)
-                                            {
-                                                string name = (string)att.Attribute("name");
-                                                string value = att.Value;
-
-                                                ufedProjectAttributes.ExtractionData.Add(Tuple.Create(name, value));
-                                            }
-                                        }
-                                        else if (reader.Depth == 1 && reader.Name == "metadata" && reader.GetAttribute("section") == "Device Info")
-                                        {
-                                            XmlReader attReader = reader.ReadSubtree();
-                                            XElement attNode = XElement.Load(attReader);
-
-                                            IEnumerable<XElement> atributes = attNode.Descendants();
-
-                                            foreach (XElement att in atributes)
-                                            {
-                                                string name = (string)att.Attribute("name");
-                                                string value = att.Value;
-
-                                                ufedProjectAttributes.DeviceInfo.Add(Tuple.Create(name, value));
-                                            }
-                                        }
-                                        else if (reader.Depth == 1 && reader.Name == "caseInformation")
-                                        {
-                                            XmlReader attReader = reader.ReadSubtree();
-                                            XElement attNode = XElement.Load(attReader);
-
-                                            IEnumerable<XElement> atributes = attNode.Descendants();
-
-                                            foreach (XElement att in atributes)
-                                            {
-                                                string name = (string)att.Attribute("name");
-                                                string value = att.Value;
-
-                                                ufedProjectAttributes.CaseInformation.Add(Tuple.Create(name, value));
-                                            }
-                                        }
-
-                                        if (progress != null)
-                                        {
-                                            if (reportSize > 0)
-                                            {
-                                                currentPosition = sr.BaseStream.Position;
-                                                int percent = (int)((double)currentPosition / reportSize * 100);
-                                                if (percent > lastPercent)
-                                                {
-                                                    lastPercent = percent;
-                                                    progress.Report(percent);
-                                                }
-                                            }
-                                        }
-                                    }
-                                    catch (Exception ex)
-                                    {
-                                        Console.WriteLine("Error parsing project attributes: " + ex.Message);
-                                    }
-                                }
-
-                                if (progress != null)
-                                {
-                                    progress.Report(100);
-                                }
-                                return ufedProjectAttributes;
-                            }
+                            Console.WriteLine("report.xml not found in the ufdr file");
+                            return null;
                         }
+                       
+                        using (Stream reportStream = report.Open())
+                        {
+                            return ParseProjectAttributes(reportStream, reportSize, progress);
+                        }
+                    }
+                }
+                else if (Path.GetExtension(filename).ToLower() == ".xml")
+                {
+                    using (FileStream fs = new FileStream(filename, FileMode.Open, FileAccess.Read))
+                    {
+                        return ParseProjectAttributes(fs, reportSize, progress);
                     }
                 }
             }
@@ -196,6 +88,150 @@ namespace UFEDLib
             }
 
             return null;
+        }
+
+        private static ProjectAttributes ParseProjectAttributes( Stream stream, long reportSize, IProgress<int> progress = null)
+        {
+            long currentPosition = 0;
+            int lastPercent = 0;
+
+
+            using (StreamReader sr = new StreamReader(stream))
+            {
+                XmlReaderSettings xmlReaderSettings = new XmlReaderSettings
+                {
+                    CheckCharacters = false
+                };
+
+                using (XmlReader reader = XmlReader.Create(sr, xmlReaderSettings))
+                {
+                    ProjectAttributes ufedProjectAttributes = new ProjectAttributes();
+
+                    var nsmgr = new XmlNamespaceManager(new NameTable());
+                    nsmgr.AddNamespace("a", "http://pa.cellebrite.com/report/2.0");
+                    XNamespace xNamespace = "http://pa.cellebrite.com/report/2.0";
+
+                    bool readProjectAttributes = false;
+                    bool readAddítionalFields = false;
+                    bool readExtractionData = false;
+                    bool readDeviceInfo = false;
+                    bool readCaseInformation = false;
+
+                    while (reader.Read())
+                    {
+                        try
+                        {
+                            if (reader.Name == "project" && reader.NodeType != XmlNodeType.EndElement)
+                            {
+                                ufedProjectAttributes.ProjectId = reader.GetAttribute("id");
+                                ufedProjectAttributes.ProjectName = reader.GetAttribute("name");
+                                ufedProjectAttributes.NodeCount = int.Parse(reader.GetAttribute("NodeCount"));
+                                ufedProjectAttributes.ModelCount = int.Parse(reader.GetAttribute("ModelCount"));
+                                ufedProjectAttributes.ReportVersion = reader.GetAttribute("reportVersion");
+                                readProjectAttributes = true;
+                            }
+
+                            if (reader.Depth == 1 && reader.Name == "metadata" && reader.GetAttribute("section") == "Additional Fields")
+                            {
+                                XmlReader attReader = reader.ReadSubtree();
+                                XElement attNode = XElement.Load(attReader);
+
+                                IEnumerable<XElement> atributes = attNode.Descendants();
+
+                                foreach (XElement att in atributes)
+                                {
+                                    string name = (string)att.Attribute("name");
+                                    string value = att.Value;
+
+                                    ufedProjectAttributes.AdditionalFields.Add(Tuple.Create(name, value));
+                                }
+                                readAddítionalFields = true;
+
+                            }
+                            else if (reader.Depth == 1 && reader.Name == "metadata" && reader.GetAttribute("section") == "Extraction Data")
+                            {
+                                XmlReader attReader = reader.ReadSubtree();
+                                XElement attNode = XElement.Load(attReader);
+
+                                IEnumerable<XElement> atributes = attNode.Descendants();
+
+                                foreach (XElement att in atributes)
+                                {
+                                    string name = (string)att.Attribute("name");
+                                    string value = att.Value;
+
+                                    ufedProjectAttributes.ExtractionData.Add(Tuple.Create(name, value));
+                                }
+
+                                readExtractionData = true;
+                            }
+                            else if (reader.Depth == 1 && reader.Name == "metadata" && reader.GetAttribute("section") == "Device Info")
+                            {
+                                XmlReader attReader = reader.ReadSubtree();
+                                XElement attNode = XElement.Load(attReader);
+
+                                IEnumerable<XElement> atributes = attNode.Descendants();
+
+                                foreach (XElement att in atributes)
+                                {
+                                    string name = (string)att.Attribute("name");
+                                    string value = att.Value;
+
+                                    ufedProjectAttributes.DeviceInfo.Add(Tuple.Create(name, value));
+                                }
+
+                                readDeviceInfo = true;
+                            }
+                            else if (reader.Depth == 1 && reader.Name == "caseInformation")
+                            {
+                                XmlReader attReader = reader.ReadSubtree();
+                                XElement attNode = XElement.Load(attReader);
+
+                                IEnumerable<XElement> atributes = attNode.Descendants();
+
+                                foreach (XElement att in atributes)
+                                {
+                                    string name = (string)att.Attribute("name");
+                                    string value = att.Value;
+
+                                    ufedProjectAttributes.CaseInformation.Add(Tuple.Create(name, value));
+                                }
+
+                                readCaseInformation = true;
+                            }
+
+                            if (progress != null)
+                            {
+                                if (reportSize > 0)
+                                {
+                                    currentPosition = sr.BaseStream.Position;
+                                    int percent = (int)((double)currentPosition / reportSize * 100);
+                                    if (percent > lastPercent)
+                                    {
+                                        lastPercent = percent;
+                                        progress.Report(percent);
+                                    }
+                                }
+                            }
+
+                            if (readProjectAttributes && readAddítionalFields && readExtractionData && readDeviceInfo && readCaseInformation)
+                            {
+                                break;
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine("Error parsing project attributes: " + ex.Message);
+                        }
+                    }
+
+                    if (progress != null)
+                    {
+                        progress.Report(100);
+                    }
+                    return ufedProjectAttributes;
+                }
+            }
         }
     }
 }
